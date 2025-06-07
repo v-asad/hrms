3316148626134f0f94ceda093d3c3f52da037f33
v-asad <asadsohailabid@gmail.com>
2025-06-07 23:14:58 +0500

    Fixed library issues with jwt and bcrypt

--- src/lib/jwt.ts
+++ src/lib/jwt.ts
@@ -3,11 +3,11 @@ import jwt from "jsonwebtoken";
 const JWT_SECRET = process.env.JWT_SECRET!;
 const JWT_REFRESH_SECRET = process.env.JWT_REFRESH_SECRET!;
 
-export function signAccessToken(payload: object, expiresIn = "15m") {
+export function signAccessToken(payload: object, expiresIn = 9000) {
   return jwt.sign(payload, JWT_SECRET, { expiresIn });
 }
 
-export function signRefreshToken(payload: object, expiresIn = "7d") {
+export function signRefreshToken(payload: object, expiresIn = 10080) {
   return jwt.sign(payload, JWT_REFRESH_SECRET, { expiresIn });
 }
 
